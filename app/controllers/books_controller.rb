07f19aae36e64046c2ea5af21d17edbf8ab0be3a
Dan Kleiman <info@dankleiman.com>
2014-10-26 13:46:04 -0400

    added pagination

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @sections = @book.sections.paginate(:page => params[:page])
   end
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,4 @@
 class Section < ActiveRecord::Base
   belongs_to :book
+  self.per_page = 1
 end
