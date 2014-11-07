b45748ae38ccaf4565b4f5e7fd336fb42ca718ad
Dan Kleiman <info@dankleiman.com>
2014-11-07 08:43:54 -0500

    ajax form submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def subscribe
     begin
-      Gibbon::API.new(ENV['MAILCHIMP_API_KEY']).lists.subscribe({:id => ENV['MAILCHIMP_LIST_ID'], :email => {:email => params[:email]}, :double_optin => false})
+      Gibbon::API.new(ENV['MAILCHIMP_API_KEY']).lists.subscribe({:id => ENV['MAILCHIMP_LIST_ID'], :email => {:email => params[:user][:email]}, :double_optin => false})
       flash[:notice] = "You have successfully subscribed!"
     rescue Gibbon::MailChimpError => e
       flash[:alert] = e.message
     end
-    redirect_to root_path
+    render nothing: true
   end
 end
