d811f360f82e0fe51c0a020b39d46ae16fd5eb7c
Dan Kleiman <info@dankleiman.com>
2014-11-10 08:25:36 -0500

    flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def subscribe
     begin
       Gibbon::API.new(ENV['MAILCHIMP_API_KEY']).lists.subscribe({:id => ENV['MAILCHIMP_LIST_ID'], :email => {:email => params[:user][:email]}, :double_optin => false})
-      flash[:notice] = "You have successfully subscribed!"
+      flash.now[:notice] = "You have successfully subscribed!"
     rescue Gibbon::MailChimpError => e
-      flash[:alert] = e.message
+      flash.now[:alert] = e.message
     end
     render nothing: true
   end
