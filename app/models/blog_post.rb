8fe7bc2014659fa4973bf8c8fcfd0d6245c18dd3
Dan Kleiman <info@dankleiman.com>
2014-11-11 08:27:57 -0500

    admin login/logout

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -23,7 +23,7 @@ class BlogPost < ActiveRecord::Base
     if BlogPost.find_by(section: post_section)
       BlogPost.generate_blog_post!
     else
-      blog_post = BlogPost.create!(title: post_title, post_author_id: 1, approved: false, section: post_section)
+      blog_post = BlogPost.create!(title: post_title, post_author_id: 1, approved: false, section: post_section, published_at: (BlogPost.last.published_at + 1) )
       puts "CREATING BLOG POST FROM #{post_title}"
       puts "STARTING AT SECTION #{post_section.position}"
       line_count = 0
